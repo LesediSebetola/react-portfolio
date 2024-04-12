8bf1aa22cfa5da8dfebb4ebe4602f7cb8cc0e0f3
Lesedi Sebetola <lesedisebetola@gmail.com>
2024-04-12 17:06:36 +0200

    second commit

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -26,21 +26,7 @@ const Experience = () => {
         </div>
         <div>
          
-              <div className='mb-8 flex fle-wrap lg:justify-center'>
-                <div className='w-full lg:w-1/4'>
-                   <p className='mb-2 text-sm text-neutral-400'>2024 - Present</p>
-                </div>
-                <div className='w-full max-w-xl lg:w-3/4'>
-                  <h6 className='mb-2 font-semibold'>
-                  Freelance Frontend Developer
-                    <span className='text-sm text-purple-100'>Growit MC</span>
-                  </h6>
-                  <p className='mb-4 text-neutral-400'>An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.</p>
-                  
-                    <span className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>React.js</span>
-                    <span className='mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>React.js</span>
-                </div>
-              </div>
+              
        
         </div>
     </div>
